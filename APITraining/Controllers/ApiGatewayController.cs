8f85a26624dd3d5b002b6daa21a5490e93094274
Sarra Tavakoli <sarra.tavakoli@gmail.com>
2022-10-31 15:50:44 -0500

    GetByCountryStatusAndDates added, specifically for hard coded parameters

--- APITraining/Controllers/ApiGatewayController.cs
+++ APITraining/Controllers/ApiGatewayController.cs
@@ -26,11 +26,34 @@ namespace APITraining.Controllers
                     return BadRequest("Something went wrong in the request. Please check the request Uri.");
                 }
             }
-            catch(Exception ex) { return Ok(ex.Message); }
+            catch (Exception ex) { return Ok(ex.Message); }
             finally { }
         }
 
-        
+        [HttpGet]
+        [Route("~/apigateway/GetByCountryStatusAndDates")]
+        public IActionResult GetByCountryStatusAndDates()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("https://api.covid19api.com/");
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = client.GetAsync("total/country/united-states/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return Ok(response.Content.ReadAsStringAsync().Result);
+                }
+                else
+                {
+                    return BadRequest("Something went wrong in the request. Please check the request Uri.");
+                }
+            }
+            catch (Exception ex) { return Ok(ex.Message); }
+            finally { }
+        }
 
     }
 }
